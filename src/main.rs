7d9fa8aeeff133f6e7368fbfe2d8576af8c2fe8b
Aman Singh <singhaman11415@gmail.com>
2024-07-19 18:44:04 +0530

    added more exercises

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 /*exercise-1 run hello world*/
 use std::any::type_name_of_val;
+use std::ops::Index;
 
 fn hello_world() {
     print!("Hello,");
@@ -149,13 +150,171 @@ fn pattern_matching() {
     }
 }
 
+fn rust_arrays() {
+//     array declaration
+    let nums = [1, 2, 3, 4, 5];
+    let nums2: [i8; 10] = [1,2,3,4,5,6,7,8,9,10];
+    let nums3 = [1; 5]; // [1, 1, 1, 1, 1] array with 5 elements with value 1
+    // an array without data type
+    let a = [5, 4, 3, 2, 1];
+
+    // an array with data type and size
+    let b: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // an array with default values
+    let c = [3; 5];
+
+    println!("a = {a:?}");
+    println!("b = {:?}", b);
+    println!("c = {:?}", c);
+
+//     access array elements
+    let first = a[0];
+    let second = a[1];
+    println!("First: {first}, Second: {second}");
+
+//     get array length
+    let length = a.len();
+    println!("Length of a: {length}");
+
+//     change array elements
+    let mut d = [1, 2, 3, 4, 5];
+    d[0] = 10;
+    println!("d = {:?}", d);
+
+//     slicing array
+    let after_three = &d[3..];
+    let before_three = &d[..3];
+    let between_one_four = &d[1..];
+    println!("After three: {after_three:?}");
+    println!("Before three: {before_three:?}");
+    println!("Between one and four: {between_one_four:?}");
+}
+
+fn mutable_slice_array() {
+    // mutable array
+    let mut colors = ["red", "green", "yellow", "white"];
+
+    println!("array = {:?}", colors);
+
+    // mutable slice
+    let sliced_colors = &mut colors[1..3];
+
+    println!("original slice = {:?}", sliced_colors);
+
+    // change the value of the original slice at the first index
+    sliced_colors[1] = "purple";
+
+    println!("changed slice = {:?}", sliced_colors);
+}
+
+fn rust_tuples() {
+    let random_tuple = ("Hello", 200, 3.14);
+
+    // accessing tuple element at index 0
+    println!("Value at Index 0 = {}", random_tuple.0);
+
+    // accessing tuple element at index 1
+    println!("Value at Index 1 = {}", random_tuple.1);
+
+    // accessing tuple element at index 2
+    println!("Value at Index 2 = {}", random_tuple.2);
+
+//     destructure tuple
+    let (name, value, pi) = random_tuple;
+    println!("Name: {name}, Value: {value}, Pi: {pi}");
+}
+
+fn rust_structures() {
+    struct Person {
+        name: String,
+        age: i32,
+        height: f32,
+    }
+
+    let person = Person {
+        name: "Aman Singh".to_string(),
+        age: 21,
+        height: 31.5,
+    };
+
+    println!("Name: {0}, Age: {1}, Height: {2}", person.name, person.age, person.height);
+}
+
+fn rust_closure() {
+//     same as anonymous functions or lambda functions
+    let add = |a: i32, b: i32| a + b;
+    let result = add(5, 10);
+    println!("Result: {result}");
+
+    // define a multi-line closure
+    let squared_sum = |x: i32, y: i32| {
+
+        // find the sum of two parameters
+        let mut sum: i32 = x + y;
+
+        // find the squared value of the sum
+        let mut result: i32 = sum * sum;
+
+        return result;
+    };
+
+    // call the closure
+    let result = squared_sum(5, 3);
+
+    println!("Result = {}", result);
+
+}
+
+fn mutable_closure() {
+    let mut word = String::from("Hello");
+
+    // mutable closure
+    let mut print_str = || {
+        // value of word is changed here
+        word.push_str(" World!");
+        println!("word = {}", word);
+    };
+
+    // cannot immutable borrow because the variable is borrowed as mutable inside the closure
+    // println!("length of word = {}", word.len());
+
+    print_str();
+
+    // can immutable borrow because the closure has been already used
+    println!("length of word = {}", word.len());
+}
+
+fn immutable_closure() {
+    let word = String::from("Hello");
+
+    // immutable closure
+    let print_str = || {
+        // word variable is moved to a new variable
+        let new_word = word;
+        println!("word = {}", new_word);
+    };
+
+    print_str();
+
+    // cannot immutable borrow because word variable has moved inside closure
+    // println!("length of word = {}", word.len());
+}
+
 fn main() {
-    hello_world();
-    print_variables();
-    rust_variables();
-    type_casting();
-    arithmetic();
-    conditions();
-    loops();
-    pattern_matching()
+    // hello_world();
+    // print_variables();
+    // rust_variables();
+    // type_casting();
+    // arithmetic();
+    // conditions();
+    // loops();
+    // pattern_matching();
+    // rust_arrays();
+    // mutable_slice_array();
+    // rust_tuples();
+    // rust_structures();
+    // rust_closure();
+    // mutable_closure();
+    immutable_closure();
 }
